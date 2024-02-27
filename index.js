a4d24a0ca7998a7d1c500a1977725b833d9c9703
clasen <martin.clasen@gmail.com>
2024-02-27 13:19:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const { createClient } = require('redis');
-const { customAlphabet } = require('nanoid');
+const { createClient } = require("redis");
+const { customAlphabet } = require("nanoid");
 
 class DeepBaseRedis {
 
@@ -9,8 +9,8 @@ class DeepBaseRedis {
         this.name = "db";
         this.url = "redis://localhost:6379";
         this.client = createClient({ url: this.url });
-        this.nanoid = customAlphabet(this.nidAlphabet, this.nidLength)
-        Object.assign(this, opts)
+        this.nanoid = customAlphabet(this.nidAlphabet, this.nidLength);
+        Object.assign(this, opts);
     }
 
     async disconnect() {
@@ -22,14 +22,14 @@ class DeepBaseRedis {
     }
 
     async set(...args) {
-        if (args.length < 2) return
-        const keys = args.slice(0, -1)
-        const value = args[args.length - 1]
+        if (args.length < 2) return;
+        const keys = args.slice(0, -1);
+        const value = args[args.length - 1];
 
-        const key = keys.shift()
-        const keyPath = keys.length == 0 ? "." : keys.join('.')
+        const key = keys.shift();
+        const keyPath = keys.length == 0 ? "." : keys.join(".");
         await this._set(key, keyPath, value);
-        return args.slice(0, -1)
+        return args.slice(0, -1);
     }
 
     async _set(key, path, value) {
@@ -49,7 +49,7 @@ class DeepBaseRedis {
         if (r === null) {
             const keys = path.split('.');
             keys.pop();
-            const keyPath = keys.length == 0 ? "." : keys.join('.')
+            const keyPath = keys.length == 0 ? "." : keys.join(".");
             await this._set(key, keyPath, {});
             await this._set(key, path, value);
         }
@@ -59,17 +59,17 @@ class DeepBaseRedis {
 
         if (args.length === 0) {
 
-            const dic = {}
+            const dic = {};
             for (let key of await this._zeroKeys()) {
                 dic[key] = await this.get(key);
             }
             return dic;
         }
 
-        const key = args.shift()
-        const path = args.length == 0 ? "." : args.join('.');
+        const key = args.shift();
+        const path = args.length == 0 ? "." : args.join(".");
 
-        let r = null
+        let r = null;
         try {
             r = await this.client.json.get(this.name + ":" + key, { path });
         } catch (error) {
@@ -79,49 +79,49 @@ class DeepBaseRedis {
     }
 
     async keys(...args) {
-        const r = await this.get(...args)
+        const r = await this.get(...args);
         return (r !== null && typeof r === "object") ? Object.keys(r) : [];
     }
 
     async values(...args) {
-        const r = await this.get(...args)
+        const r = await this.get(...args);
         return (r !== null && typeof r === "object") ? Object.values(r) : [];
     }
 
     async upd(...args) {
-        const func = args.pop()
-        return this.set(...args, func(await this.get(...args)))
+        const func = args.pop();
+        return this.set(...args, func(await this.get(...args)));
     }
 
     async inc(...args) {
 
-        const i = args.pop()
-        const key = args.shift()
-        const path = args.length == 0 ? "." : args.join('.')
+        const i = args.pop();
+        const key = args.shift();
+        const path = args.length == 0 ? "." : args.join(".");
 
         try {
             await this.client.json.numIncrBy(this.name + ":" + key, path, i);
-            return args.slice(0, -1)
+            return args.slice(0, -1);
         } catch (error) {
-            args.unshift(key)
-            return this.upd(...args, n => n + i)
+            args.unshift(key);
+            return this.upd(...args, n => n + i);
         }
     }
 
     async dec(...args) {
-        const i = args.pop()
-        args.push(-i)
-        return this.inc(...args)
+        const i = args.pop();
+        args.push(-i);
+        return this.inc(...args);
     }
 
     async _zeroKeys() {
         const scan = {
-            TYPE: 'ReJSON-RL',
-            MATCH: this.name + ':*',
+            TYPE: "ReJSON-RL",
+            MATCH: this.name + ":*",
             COUNT: 1000,
         }
 
-        const keys = []
+        const keys = [];
         for await (let key of this.client.scanIterator(scan)) {
             keys.push(key.substring(this.name.length + 1));
         }
@@ -138,23 +138,23 @@ class DeepBaseRedis {
             }
         }
 
-        const key = args.shift()
-        const path = args.length == 0 ? "." : args.join('.')
+        const key = args.shift();
+        const path = args.length == 0 ? "." : args.join(".");
         await this.client.json.del(this.name + ":" + key, path);
-        return [key, ...args]
+        return [key, ...args];
     }
 
     async add(...keys) {
         const value = keys.pop();
         const id = this.nanoid();
-        await this.set(...[...keys, id, value])
-        return [...keys, id]
+        await this.set(...[...keys, id, value]);
+        return [...keys, id];
     }
 
     use(plugin) {
         const prototype = Object.getPrototypeOf(plugin);
         Object.getOwnPropertyNames(prototype).forEach(method => {
-            if (method !== 'constructor') {
+            if (method !== "constructor") {
                 this[method] = plugin[method].bind(this);
             }
         });
